af32f5e33ea5b3d14a64ac3aad9331201102d116
Christian Schmitz <ChristianSchmitz.idk@gmail.com>
2022-10-19 14:17:50 +0200

    colored triangles

--- building/main.py
+++ building/main.py
@@ -2,6 +2,9 @@ import pygame
 import math
 
 WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
 
 
 class main:
@@ -9,12 +12,18 @@ class main:
         (width, height) = (400,400)
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
-        self.unformated = 120
+        self.unformated = 68
+        self.pitchunformated = 170
         self.heading = math.radians(self.unformated)
+        self.pitch = math.radians(self.pitchunformated)
         self.createform()
-        self.transform = Matrix([math.cos(self.heading),0, -math.sin(self.heading),
-                                 0,1,0,
-                                 math.sin(self.heading), 0, math.cos(self.heading)])
+        self.headingTransform = Matrix([math.cos(self.heading),0,math.sin(self.heading),
+                                        0,1,0,
+                                        -math.sin(self.heading),0, math.cos(self.heading)])
+        self.pitchtransform = Matrix([1,0,0,
+                                      0,math.cos(self.pitch), math.sin(self.pitch),
+                                      0, -math.sin(self.pitch), math.cos(self.pitch)])
+        self.transform = self.headingTransform.muliply(self.pitchtransform)
 
 
     def createform(self):
@@ -22,31 +31,35 @@ class main:
         t1v1 = vertex(100, 100, 100)
         t1v2 = vertex(-100, -100, 100)
         t1v3 = vertex(-100, 100, -100)
-        t1 = Triangle(t1v1,t1v2,t1v3,WHITE)
+        t1 = Triangle(t1v1,t1v2,t1v3, WHITE)
         self.tris.append(t1)
 
         t2v1 = vertex(100, 100, 100)
         t2v2 = vertex(-100, -100, 100)
         t2v3 = vertex(100, -100, -100)
-        t2 = Triangle(t2v1, t2v2, t2v3, WHITE)
+        t2 = Triangle(t2v1, t2v2, t2v3, RED)
         self.tris.append(t2)
 
         t3v1 = vertex(-100, 100, -100)
         t3v2 = vertex(100, -100, -100)
         t3v3 = vertex(100, 100, 100)
-        t3 = Triangle(t3v1, t3v2, t3v3, WHITE)
+        t3 = Triangle(t3v1, t3v2, t3v3, GREEN)
         self.tris.append(t3)
 
         t4v1 = vertex(-100, 100, -100)
         t4v2 = vertex(100, -100, -100)
         t4v3 = vertex(-100, -100, 100)
-        t4 = Triangle(t4v1, t4v2, t4v3, WHITE)
+        t4 = Triangle(t4v1, t4v2, t4v3, BLUE)
         self.tris.append(t4)
 
-    def updatetransform(self, heading):
-        self.transform = Matrix([math.cos(heading), 0, -math.sin(heading),
-                                 0, 1, 0,
-                                 math.sin(heading), 0, math.cos(heading)])
+    def updatetransform(self, heading, pitch):
+        self.headingTransform = Matrix([math.cos(heading), 0, math.sin(heading),
+                                        0, 1, 0,
+                                        -math.sin(heading), 0, math.cos(heading)])
+        self.pitchtransform = Matrix([1, 0, 0,
+                                      0, math.cos(pitch), math.sin(pitch),
+                                      0, -math.sin(pitch), math.cos(pitch)])
+        self.transform = self.headingTransform.muliply(self.pitchtransform)
 
     def draw(self):
         self.screen.fill((0, 0, 0))
@@ -58,9 +71,40 @@ class main:
             pygame.draw.line(self.screen, WHITE, (v2.x+200, v2.y+200), (v3.x+200, v3.y+200))
             pygame.draw.line(self.screen, WHITE, (v1.x+200, v1.y+200), (v3.x+200, v3.y+200))
 
+    def coloreddrawing(self):
+        self.screen.fill((0, 0, 0))
+        for t in self.tris:
+            v1 = self.transform.transform(t.v1)
+            v2 = self.transform.transform(t.v2)
+            v3 = self.transform.transform(t.v3)
+
+            v1.x += 200
+            v1.y += 200
+            v2.x += 200
+            v2.y += 200
+            v3.x += 200
+            v3.y += 200
+
+            minX = max(0, math.ceil(min(v1.x, min(v2.x, v3.x))))
+            maxX = min(400 - 1, math.floor(max(v1.x, max(v2.x, v3.x))))
+            minY = max(0, math.ceil(min(v1.y, min(v2.y, v3.y))))
+            maxY = min(400 - 1, math.floor(max(v1.y, max(v2.y, v3.y))))
+
+            triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x)
+
+            for y in range(minY, maxY):
+                for x in range(minX, maxX):
+                    b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea
+                    b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea
+                    b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea
+                    if b1 >= 0 and b1 <= 1 and b2 >= 0 and b2 <= 1 and b3 >= 0 and b3 <= 1:
+                        self.screen.set_at((x,y), t.color)
+
+
     def run(self):
         running = True
         while running:
+            #self.coloreddrawing()
             self.draw()
             pygame.display.flip()
             for event in pygame.event.get():
@@ -68,13 +112,21 @@ class main:
                     running = False
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT]:
-                self.unformated += 0.01
+                self.unformated += 5
                 self.heading = math.radians(self.unformated)
-                self.updatetransform(self.heading)
+                self.updatetransform(self.heading, self.pitch)
             if keys[pygame.K_RIGHT]:
-                self.unformated -= 0.01
+                self.unformated -= 5
                 self.heading = math.radians(self.unformated)
-                self.updatetransform(self.heading)
+                self.updatetransform(self.heading, self.pitch)
+            if keys[pygame.K_DOWN]:
+                self.pitchunformated += 5
+                self.pitch = math.radians(self.pitchunformated)
+                self.updatetransform(self.heading, self.pitch)
+            if keys[pygame.K_UP]:
+                self.pitchunformated -= 5
+                self.pitch = math.radians(self.pitchunformated)
+                self.updatetransform(self.heading, self.pitch)
 
 
 
@@ -95,7 +147,7 @@ class Matrix:
     def __init__(self, values):
         self.values = values
     def muliply(self, other):
-        result = []
+        result = [0] * 9
         for row in range(3):
             for col in range(3):
                 for i in range(3):
